78aae2f0324002935d8f8d2eba4bdc7650a15480
Kevin <kevgleeson78@gmail.com>
2016-04-11 17:40:31 +0100

    ROuting added with params for individual product

--- APM/app/products/product-list.component.ts
+++ APM/app/products/product-list.component.ts
@@ -1,21 +1,21 @@
 import { Component, OnInit } from 'angular2/core';
+import { ROUTER_DIRECTIVES } from 'angular2/router';
 import { IProduct } from './product';
 import {ProductFilterPipe } from './product-filter.pipe';
 import { StarComponent } from '../shared/star.component';
 import { ProductService } from './product.service';
 @Component({
-    selector: 'pm-products',
     templateUrl: 'app/products/product-list.component.html',
     styleUrls: ['app/products/product-list.component.css'],
     pipes: [ProductFilterPipe],
-    directives: [StarComponent]
+    directives: [StarComponent, ROUTER_DIRECTIVES]
 })
 export class ProductListComponent implements OnInit{
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = 'cart';
+    listFilter: string;
     errorMessage: string;
     
     products: IProduct[];
